619f6bd4254a3a56568d9804f31f9429ca57f34b
nirupg63 <nirupg63@bitbucket.org>
2018-06-27 01:08:26 +0530

    init project

---
+++ app/src/main/java/org/vortex/vortexchat/MainActivity.java
@@ -0,0 +1,56 @@
+package org.vortex.vortexchat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+    // Choose an arbitrary request code value
+    private static final int RC_SIGN_IN = 123;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        if (auth.getCurrentUser() != null) {
+            // already signed in
+            startActivity(new Intent(this, SuccessActivity.class));
+        } else {
+            // not signed in
+            AuthUI.IdpConfig phoneConfigWithDefaultNumber = new AuthUI.IdpConfig.PhoneBuilder()
+                    .setDefaultCountryIso("in")
+                    .build();
+
+            startActivityForResult(
+                    // Get an instance of AuthUI based on the default app
+                    AuthUI.getInstance().createSignInIntentBuilder()
+                            .setAvailableProviders(Arrays.asList(
+                                    new AuthUI.IdpConfig.PhoneBuilder().build()))
+                            .setTosUrl("https://superapp.example.com/terms-of-service.html")
+                            .setPrivacyPolicyUrl("https://superapp.example.com/privacy-policy.html")
+                            .setIsSmartLockEnabled(false) // Enable this on release
+                            .build(),
+                    RC_SIGN_IN);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse idpResponse = IdpResponse.fromResultIntent(data);
+            startActivity(new Intent(this, SuccessActivity.class)
+                    .putExtra("my_token", idpResponse.getIdpToken()));
+        }
+    }
+}
